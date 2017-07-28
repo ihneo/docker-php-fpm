6f2a1a4679ee33a2d50914842588a6dc08fd0d24
Nicolas Alscher <nicolas.alscher@ihneo.fr>
2017-07-28 17:38:31 +0200

    PC project

---
+++ 5.5-fpm-alpine-pc/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Newrelic extension
+if [ "$APP_ENVIRONMENT" != "dev" ] && [ "$APP_REGION" != "us1" ]
+then
+	sed -i 's/^;\(newrelic\.license = "\).*\("\)/\1'$NEWRELIC_KEY'\2 /' /usr/local/etc/php/conf.d/newrelic.ini
+	sed -i 's/^\(newrelic\.appname = "\).*\("\)/\1API ('$APP_ENVIRONMENT_DISPLAY')\2 /' /usr/local/etc/php/conf.d/newrelic.ini
+	sed -i 's/^;\(newrelic\.labels = "\).*\("\)/\1Environment:'$APP_ENVIRONMENT_DISPLAY'\2 /' /usr/local/etc/php/conf.d/newrelic.ini
+	cp /usr/local/etc/php/conf.d/newrelic.ini /usr/local/etc/php-fpm.d/
+else
+	/bin/rm -f /usr/local/etc/php/conf.d/newrelic.ini
+fi
+
+# Exec php-fpm
+exec php-fpm
