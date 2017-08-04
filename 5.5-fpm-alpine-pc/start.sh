67851f722d7c1a1a78a4b465c83cf3381c97e18c
Nicolas Alscher <nicolas.alscher@ihneo.fr>
2017-08-04 12:21:13 +0200

    Add APP_DEPLOY_ENV

--- 5.5-fpm-alpine-pc/start.sh
+++ 5.5-fpm-alpine-pc/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Newrelic extension
-if [ "$APP_ENVIRONMENT" != "dev" ] && [ "$APP_REGION" != "us1" ]
+if [ "$APP_ENVIRONMENT" != "dev" ] && [ "$APP_REGION" == "us1" ]
 then
 	sed -i 's/^;\(newrelic\.license = "\).*\("\)/\1'$NEWRELIC_KEY'\2 /' /usr/local/etc/php/conf.d/newrelic.ini
 	sed -i 's/^\(newrelic\.appname = "\).*\("\)/\1API ('$APP_DEPLOY_ENV')\2 /' /usr/local/etc/php/conf.d/newrelic.ini
