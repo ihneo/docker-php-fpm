2749a5bfb721c0ecfcb346d06ae41a1197a8f9cf
Nicolas Alscher <nicolas.alscher@ihneo.fr>
2017-08-09 15:45:31 +0200

    modify sed for NR

--- 5.5-fpm-alpine-pc/start.sh
+++ 5.5-fpm-alpine-pc/start.sh
@@ -3,7 +3,7 @@
 # Install Newrelic extension
 if [ "$APP_ENVIRONMENT" != "dev" ] && [ "$APP_REGION" == "us1" ]
 then
-	sed -i 's/^;\(newrelic\.license = "\).*\("\)/\1'$NEWRELIC_KEY'\2 /' /usr/local/etc/php/conf.d/newrelic.ini
+	sed -i 's/^\(newrelic\.license = "\).*\("\)/\1'$NEWRELIC_KEY'\2 /' /usr/local/etc/php/conf.d/newrelic.ini
 	sed -i 's/^\(newrelic\.appname = "\).*\("\)/\1API ('$APP_DEPLOY_ENV')\2 /' /usr/local/etc/php/conf.d/newrelic.ini
 	sed -i 's/^;\(newrelic\.labels = "\).*\("\)/\1Environment:'$APP_DEPLOY_ENV'\2 /' /usr/local/etc/php/conf.d/newrelic.ini
 	cp /usr/local/etc/php/conf.d/newrelic.ini /usr/local/etc/php-fpm.d/
