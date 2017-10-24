c6b028397d1f4a99267d4ea13cfe7be575df7338
Nicolas Alscher <nicolas.alscher@ihneo.fr>
2017-10-24 18:03:38 +0200

    Newrelic

--- 5.5-fpm-alpine-pc/start.sh
+++ 5.5-fpm-alpine-pc/start.sh
@@ -6,7 +6,7 @@ then
 	sed -i 's/^\(newrelic\.license = "\).*\("\)/\1'$NEWRELIC_KEY'\2 /' /usr/local/etc/php/conf.d/newrelic.ini
 	sed -i 's/^\(newrelic\.appname = "\).*\("\)/\1API ('$APP_DEPLOY_ENV')\2 /' /usr/local/etc/php/conf.d/newrelic.ini
 	sed -i 's/^;\(newrelic\.labels = "\).*\("\)/\1Environment:'$APP_DEPLOY_ENV'\2 /' /usr/local/etc/php/conf.d/newrelic.ini
-	sed -i 's/^;\(newrelic\.webtransaction\.name\.files = "\).*\("\)/\1\/index.php\2 /' /usr/local/etc/php/conf.d/newrelic.ini
+	sed -i 's/^;\(newrelic\.webtransaction\.name\.files = "\).*\("\)/\1\/.*\2 /' /usr/local/etc/php/conf.d/newrelic.ini
 	cp /usr/local/etc/php/conf.d/newrelic.ini /usr/local/etc/php-fpm.d/
 else
 	/bin/rm -f /usr/local/etc/php/conf.d/newrelic.ini
